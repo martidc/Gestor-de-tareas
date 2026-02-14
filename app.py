c05f6ed9e5cd270dc066915b0e3a22f192092eec
Martina de la Cámara <108492024+martidc@users.noreply.github.com>
2026-02-14 14:34:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,6 @@ def eliminar_tarea(id):
     return jsonify({'mensaje': 'Tarea eliminada correctamente'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
